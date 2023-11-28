4c74578716e02c20f0cd078daeaf11c844da47df
Ayush Kumar <ayushspidey@gmail.com>
2023-11-29 00:24:31 +0530

    components setup

---
+++ mernapp/src/screens/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar.tsx'
+import Footer from '../components/Footer.tsx'
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <Navbar/>
+    </div>
+    <div>Body</div>
+    <div><Footer/></div>
+    </>
+    
+
+  )
+}
