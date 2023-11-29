5591e0d5a7904205a249101ac260251978978d12
Ayush Kumar <ayushspidey@gmail.com>
2023-11-30 00:26:32 +0530

    route

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 import Home from './screens/Home.tsx';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
   return (
-    <>
-      <div ><Home/></div>
-    </>
+    <Router>
+      <div >
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
