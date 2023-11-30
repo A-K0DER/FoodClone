2e449e220e001502061bd1266f2b8dd70e85f37c
Ayush Kumar <ayushspidey@gmail.com>
2023-11-30 20:09:52 +0530

    Ui etup done

--- mernapp/src/components/Card.tsx
+++ mernapp/src/components/Card.tsx
@@ -4,7 +4,7 @@ export default function Card() {
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src="https://media.istockphoto.com/id/1457847465/photo/gorditas-are-a-mexican-dish-made-from-corn-or-wheat-dough-stuffed-with-pork-rinds-beans-or.jpg?s=2048x2048&w=is&k=20&c=iEGBuPJJv3Un3Ruf6_uVlEmQwNfZYPDnM4nWkt9KSoY=" className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Food item</p>
--- mernapp/src/screens/Home.tsx
+++ mernapp/src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar.tsx";
 import Footer from "../components/Footer.tsx";
 import Card from "../components/Card.tsx";
+import Carousel from "../components/Carousel.tsx";
 
 export default function Home() {
   return (
@@ -10,6 +11,21 @@ export default function Home() {
         <Navbar />
       </div>
       <div>
+        <Carousel/>
+      </div>
+      <div className="m-3">
+        <Card/>
+      </div>
+      <div className="m-3">
+        <Card/>
+      </div>
+      <div className="m-3">
+        <Card/>
+      </div>
+      <div className="m-3">
+        <Card/>
+      </div>
+      <div className="m-3">
         <Card/>
       </div>
       <div>
